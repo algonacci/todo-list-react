ef57a432b09702587803c5580a4ccf452b6e24cb
Eric Julianto <algonacci1@gmail.com>
2023-08-04 16:38:31 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/todos");
+      setTodos(response.data.data);
+    } catch (error) {
+      console.error("Error fetching Todos:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (title.trim() === "") return;
+
+    try {
+      const response = await axios.post("http://localhost:8080/todos", {
+        title: title,
+        description: description,
+        completed: false,
+      });
+
+      setTodos([...todos, response.data.data]);
+      setTitle("");
+      setDescription("");
+    } catch (error) {
+      console.error("Error creating Todo:", error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:8080/todos/${id}`);
+      setTodos(todos.filter((todo) => todo.ID !== id));
+    } catch (error) {
+      console.error("Error deleting Todo:", error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Todo App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={title}
+          onChange={handleTitleChange}
+          placeholder="Enter a new todo..."
+        />
+        <input
+          type="text"
+          value={description}
+          onChange={handleDescriptionChange}
+          placeholder="Enter description..."
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.ID}>
+            <strong>{todo.title}</strong>
+            <p>{todo.description}</p>
+            <button onClick={() => handleDelete(todo.ID)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
